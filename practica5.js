6183bfc9bc1ba4a8b602f6199423ca191e3bcdd4
cletooo <128733571+cletooo@users.noreply.github.com>
2023-05-18 10:03:25 -0600

    Add files via upload

---
+++ practica5.js
@@ -0,0 +1,23 @@
+//Funciones
+function edad(birthYear){
+        const edad = 2023 - birthYear
+        return edad
+}
+const edadDiego = edad(1997)
+console.log(edadDiego)
+
+
+//Funcion de potencia
+ function potencia(num,exp){
+const exp=4
+const num =3
+const r = 1;
+let i = 0; 
+while(i < exp){
+    r = r * num;
+    i++;
+} 
+return potencia
+ }
+ 
+ console.log(r)
